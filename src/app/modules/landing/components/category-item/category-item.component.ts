32e03332da877799caf9f53d904c877631aa3c66
Sony <jsaulhernandez98@gmail.com>
2024-10-19 12:23:35 -0600

    tag: adding category component to home view

---
+++ src/app/modules/landing/components/category-item/category-item.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ss-category-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './category-item.component.html',
+  styleUrl: './category-item.component.scss',
+})
+export class CategoryItemComponent {}
