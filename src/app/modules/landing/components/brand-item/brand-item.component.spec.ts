5b2b3d32eab7bbd9cffeb8c50ee5a0896b756369
Sony <jsaulhernandez98@gmail.com>
2024-10-20 10:00:58 -0600

    tag: adding UI to brand item

---
+++ src/app/modules/landing/components/brand-item/brand-item.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BrandItemComponent } from './brand-item.component';
+
+describe('BrandItemComponent', () => {
+  let component: BrandItemComponent;
+  let fixture: ComponentFixture<BrandItemComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [BrandItemComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(BrandItemComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/modules/landing/components/brand-item/brand-item.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-brand-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './brand-item.component.html',
+  styleUrl: './brand-item.component.scss'
+})
+export class BrandItemComponent {
+
+}
