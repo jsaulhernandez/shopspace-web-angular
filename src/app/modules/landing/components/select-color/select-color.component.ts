7d1bed4b5116dc2c65266f60519f3a0b802cb35c
Sony <jsaulhernandez98@gmail.com>
2024-10-27 12:08:02 -0600

    tag: adding component to show colors available

---
+++ src/app/modules/landing/components/select-color/select-color.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-select-color',
+  standalone: true,
+  imports: [],
+  templateUrl: './select-color.component.html',
+  styleUrl: './select-color.component.scss',
+})
+export class SelectColorComponent {
+  onSelectedColor() {}
+}
