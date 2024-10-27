ad948035635333a98edd8933c30b7f310b475adc
Sony <jsaulhernandez98@gmail.com>
2024-10-27 14:39:18 -0600

    tag: adding class to disable icon

--- src/app/shared/components/ss-custom-quantity/ss-custom-quantity.component.ts
+++ src/app/shared/components/ss-custom-quantity/ss-custom-quantity.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'ss-custom-quantity',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './ss-custom-quantity.component.html',
   styleUrl: './ss-custom-quantity.component.scss',
 })
@@ -18,5 +19,13 @@ export class SsCustomQuantityComponent {
     if (this.selectedQuantity === 1) return;
 
     this.selectedQuantity--;
+  }
+
+  get isQuantityEqualOne() {
+    return this.selectedQuantity === 1;
+  }
+
+  get isQuantityEqualMaxAvailable() {
+    return this.selectedQuantity === 5;
   }
 }
